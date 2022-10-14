f812f92219fcc406ade6d29665de7b8c35ccc48d
Bailey Cage <bailey.cage@gmail.com>
2022-10-14 17:20:13 -0400

    commit tree b223a23ebc4e6520cbfc1a7348d22a34c98f44ee

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record_message
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find_by!(id: params[:id])
+    render json: item, except: [:created_at, :updated_at]
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, except: [:created_at, :updated_at], status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def no_record_message
+    render json: { error: 'no user found' }, status: :not_found
   end
 
 end
